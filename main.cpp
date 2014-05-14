d2506e964799808a0a111c34c52babb377bb8ab0
wcuibo <wcuibo00@163.com>
2014-05-14 10:34:16 +0800

    add file main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	printf( "argc = %d \n", argc );
+	
+	for( int i=0; i<argc; i++ ){
+		printf( "i = %d, argv[%d] = %s \n", i,i,argv[i] );
+	}
+	return 0;
+
+//test 0 
+}
